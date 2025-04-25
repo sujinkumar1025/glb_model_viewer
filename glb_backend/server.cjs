169ca797a010d08db96656d1712fc969757d2c79
sujinkumar1025 <sujin123>
2025-04-25 12:57:44 +0530

    some updations

--- glb_backend/server.cjs
+++ glb_backend/server.cjs
@@ -13,12 +13,9 @@ const app = express();
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch(err => console.error('Connection error:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB:', err));
 
 // Middleware
 app.use(cors());
