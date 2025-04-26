49e5083e59fc114886b3ac18b1af8cb0c54cdf9e
sujinkumar1025 <sujin123>
2025-04-26 17:50:42 +0530

    upload

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -18,7 +18,8 @@ export default function UploadPage() {
   //   const apiUrl = process.env.NODE_ENV === 'production'
   // ? 'https://glb-model-viewer-1.onrender.com/models'  // Replace with the actual deployed backend URL
   // : 'http://localhost:5000/models';  // For local development
-  const apiUrl = process.env.VITE_API_URL || 'http://localhost:5000';  // Default to localhost for local dev
+  // const apiUrl = process.env.VITE_API_URL || 'http://localhost:5000';  // Default to localhost for local dev
+  const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
     try {
       await axios.post(`${apiUrl}/models`, formData, {
